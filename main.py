95492b703823838581a9bd25dc1237a2580033cd
Joshua Agustin <joshuadavide.agustinjdea@gmail.com>
2025-10-28 23:32:11 +0800

    Rename variables

--- main.py
+++ main.py
@@ -6,22 +6,22 @@ if __name__ == '__main__':
     runs = 3
     test_cases = list(range(5, 31, 5))
 
-    for i in test_cases:
-        total = 0
-        distance_matrix = matrix.gen_distance_matrix(i)
+    for case in test_cases:
+        total_time = 0
+        distance_matrix = matrix.gen_distance_matrix(case)
 
         # held karp
-        for j in range(runs):
+        for run in range(runs):
             start_time = time.perf_counter()
             result = held_karp.held_karp(distance_matrix)
             end_time = time.perf_counter()
 
             elapsed_time = end_time - start_time
-            total += elapsed_time
+            total_time += elapsed_time
 
             # print(result) may be removed
             print(f"{elapsed_time:.6f} seconds")
-        print(f"\n{i} cities average run time with Held-Karp: {total / 3} seconds\n")
+        print(f"\n{case} cities average run time with Held-Karp: {total_time / 3} seconds\n")
 
         # insert loops to run the 2 other algorithms here
         # since they need to use the same distance matrix for each test case
