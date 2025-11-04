942bf9a4fe3ca84005e5eb59bd83acd2a748d658
Joshua Agustin <joshuadavide.agustinjdea@gmail.com>
2025-11-05 01:28:47 +0800

    Refactor matrix generator

--- matrix.py
+++ matrix.py
@@ -1,13 +1,10 @@
 import random
 
 def gen_distance_matrix(n):
-    matrix = [[None for _ in range(n)] for _ in range(n)]
+    matrix = [[0 for _ in range(n)] for _ in range(n)]
     for i in range(n):
-        for j in range(n):
-            if i == j:
-                matrix[i][j] = 0
-            else:
-                rand_val = random.randint(10, 150)
-                matrix[i][j] = rand_val
-                matrix[j][i] = rand_val
+        for j in range(i + 1, n):
+            rand_val = random.randint(10, 150)
+            matrix[i][j] = rand_val
+            matrix[j][i] = rand_val
     return matrix
